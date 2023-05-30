8b8ad1b45cee7314d78b3e266b9af427b80833b3
SolusBlade <kykysha59fafa@gmail.com>
2023-05-30 21:33:13 +0300

    hw done

--- helpers/ContactValidate.js
+++ helpers/ContactValidate.js
@@ -3,19 +3,34 @@ const Joi = require("joi")
 const message = (field) => ({ "any.required": `Missing required ${field} field` });
 
 const contactAddSchema = Joi.object({
-	name: Joi.string().required().messages(message("name")),
-	email: Joi.string().required().messages(message("email")),
-	phone: Joi.string().required().messages(message("phone")),
+	// name: Joi.string().required().messages(message("name")),
+	// email: Joi.string().required().messages(message("email")),
+	// phone: Joi.string().required().messages(message("phone")),
+	name: Joi.string()
+		.required()
+		.messages({ "any.required": `Set name for contact` }),
+	email: Joi.string(),
+	phone: Joi.string(),
+	favorite: Joi.boolean(),
 });
+
 const contactUpdateSchema = Joi.object({
 	name: Joi.string(),
 	email: Joi.string(),
 	phone: Joi.string(),
+	favorite: Joi.boolean(),
 })
-	.or("name", "email", "phone")
+	.or("name", "email", "phone", "favorite")
 	.error(new Error("Missing fields"));
+
+const contactUpdateFavoriteSchema = Joi.object({
+	favorite: Joi.boolean()
+		.required()
+		.messages({ "any.required": `Missing field favorite` }),
+});
 
 module.exports = {
 	contactAddSchema,
 	contactUpdateSchema,
+	contactUpdateFavoriteSchema,
 };
--- models/contacts.js
+++ models/contacts.js
@@ -1,63 +1,30 @@
-const fs = require('fs/promises');
-const path = require('path');
-const { v4: uuidv4 } = require("uuid");
+const { Schema, model } = require("mongoose");
+const handleMongooseError = require("../helpers/handleMongooseError");
 
+const contactSchema = Schema(
+	{
+		name: {
+			type: String,
+			required: [true, "Set name for contact"],
+		},
+		email: {
+			type: String,
+		},
+		phone: {
+			type: String,
+		},
+		favorite: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	{
+		versionKey: false,
+	}
+);
 
-const contactsPath = path.join(__dirname, "contacts.json");
+contactSchema.post("save", handleMongooseError);
 
-const saveChanges = async (arr) => {
-	return await fs.writeFile(contactsPath, JSON.stringify(arr, null, 2));
-};
+const Contact = model("contact", contactSchema);
 
-const listContacts = async () => {
-  const data = await fs.readFile(contactsPath);
-	return JSON.parse(data);
-}
-
-const getContactById = async (contactId) => {
-  const contacts = await listContacts();
-	const contact =
-		(await contacts.find((item) => item.id === contactId)) || null;
-
-	return contact;
-}
-
-const removeContact = async (contactId) => {
-  const contacts = await listContacts();
-	const index = contacts.findIndex((item) => item.id === contactId);
-	if (index === -1) return null;
-	const deletedContact = contacts.splice(index, 1);
-	await saveChanges(contacts);
-
-	return deletedContact[0];
-}
-
-const addContact = async (body) => {
-  const contacts = await listContacts();
-	const newContact = {
-		id: uuidv4(),
-		...body,
-	};
-	contacts.push(newContact);
-	await saveChanges(contacts);
-
-	return newContact;
-}
-
-const updateContact = async (contactId, body) => {
-   const contacts = await listContacts();
-		const index = contacts.findIndex((item) => item.id === contactId);
-		if (index === -1) return null;
-		const updatedContact = Object.assign({ ...contacts[index], ...body });
-		contacts[index] = updatedContact;
-		await saveChanges(contacts);
-		return updatedContact;
-}
-
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
+module.exports = Contact;
