27e4ef963bbb3fc8fa948d0504d2643233d446fd
SolusBlade <kykysha59fafa@gmail.com>
2023-06-05 16:29:46 +0300

    Update ContactValidate.js

--- helpers/ContactValidate.js
+++ helpers/ContactValidate.js
@@ -1,11 +1,9 @@
 const Joi = require("joi")
 
-const message = (field) => ({ "any.required": `Missing required ${field} field` });
-
 const contactAddSchema = Joi.object({
-	name: Joi.string().required().messages(message("name")),
-	email: Joi.string().required().messages(message("email")),
-	phone: Joi.string().required().messages(message("phone")),
+	name: Joi.string().required(),
+	email: Joi.string().required(),
+	phone: Joi.string().required(),
 	favorite: Joi.boolean(),
 });
 
@@ -14,14 +12,10 @@ const contactUpdateSchema = Joi.object({
 	email: Joi.string(),
 	phone: Joi.string(),
 	favorite: Joi.boolean(),
-})
-	.or("name", "email", "phone", "favorite")
-	.error(new Error("Missing fields"));
+});
 
 const contactUpdateFavoriteSchema = Joi.object({
-	favorite: Joi.boolean()
-		.required()
-		.messages({ "any.required": `Missing field favorite` }),
+	favorite: Joi.boolean().required(),
 });
 
 module.exports = {
